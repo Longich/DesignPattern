ab2da9716be69122455716c8c39e9e5c5487ef2b
Longich <ka.kazu.kazuki@gmail.com>
2017-09-14 04:35:15 +0900

    builder practice

--- src/Builder/raw/Builder.java
+++ src/Builder/raw/Builder.java
@@ -1,8 +1,30 @@
 package Builder.raw;
 
-public interface Builder {
-    void makeTitle(String title);
-    void makeString(String str);
-    void makeItems(String[] items);
-    void close();
+public abstract class Builder {
+    boolean isTitleMade = false;
+    
+    public void makeTitle(String title) {
+        buildTitle(title);
+        isTitleMade = true;
+    };
+    public void makeString(String str) {
+        if (isTitleMade) {
+            buildString(str);
+        }
+    }
+    public void makeItems(String[] items) {
+        if (isTitleMade) {
+            buildItems(String[] items);
+        }
+    }
+
+    public void close() {
+        if (isTitleMade) {
+            endBuild();
+        }
+    }
+    public abstract void buildTitle(String title);
+    public abstract void buildString(String str);
+    public abstract void buildItems(String[] items);
+    public abstract void endBuild();
 }
